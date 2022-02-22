6c53f8b3b9430c3b62e2233b33632c3081f68f0c
Eliyas0007 <eliyassuleyman@gmail.com>
2022-02-22 15:22:13 +0000

    Training is done and modified git ignore

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
-from curses import flash
+import tqdm
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 
 # from tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter 
 
 import sys
 sys.path.append(".")
@@ -24,13 +27,16 @@ load_model = False
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# writer = SummaryWriter(f'runs/loss_plot')
+writer = SummaryWriter(f'runs/')
 step = 0
 
+# models
 encoder_cell = ConvRNNEncoderCell(hidden_size=54 * 54)
 decoder_cell = ConvRNNDecoderCell()
-
 model = Seq2Seq(encoder_cell, decoder_cell, device).to(device)
+
+# Optimizer
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 data_path = '/home/yiliyasi/Downloads/mnist_test_seq.npy'
 
@@ -39,17 +45,33 @@ dataset = MovingMNISTDataset(root_dir=data_path,
                                     transforms.Normalize((0.5), (0.5))
                                     ])
                             )
-
 loader =  DataLoader(dataset=dataset, batch_size=8, shuffle=True, num_workers=2)
 
-for e in range(num_epochs):
-    for i, data_pair in enumerate(loader):
-        train, target = data_pair
-        train = train.to(device)
-        target = target.to(device)
-        # print(target.shape)
+criterion = nn.CrossEntropyLoss()
 
-        predicted_frames = model(train)
-        # print(predicted_frames.shape)
+if load_model:
+    # TODO 
+    pass
 
-        break
+for epoch in range(num_epochs):
+
+    print(f'Epoch [{epoch / num_epochs}]')
+
+    with tqdm.tqdm(loader, unit='batch') as tepoch:
+        for data_pair in tepoch:
+            train, target = data_pair
+            train = train.to(device)
+            target = target.to(device)
+
+            predicted_frames = model(train)
+
+            optimizer.zero_grad()
+            loss = criterion(predicted_frames.to(device), target)
+            loss.backward()
+
+            nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+            optimizer.step()
+            # print(f'Training Loss : {loss.item()}')
+            writer.add_scalar('Training Loss', loss, global_step=step)
+
+            # break
